1a407dd098abf269089775ccf10a2d5b09606058
minsun <elinnane0@gmail.com>
2024-02-05 20:49:00 +0900

    :sparkles: springboot + mysql 연동

---
+++ src/test/java/kakao/school/what/WhatApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.school.what;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
