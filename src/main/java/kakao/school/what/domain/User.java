5e3fb7027b5a87c1945f25e11e9559a029396084
givemea1 <givemea4702@gmail.com>
2024-02-21 16:58:00 +0900

    :card_file_box: DATABASE VO 작성

---
+++ src/main/java/kakao/school/what/domain/User.java
@@ -0,0 +1,31 @@
+package kakao.school.what.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "sex")
+    private Integer sex;
+
+    @Column(name = "age_type")
+    private Integer ageType;
+
+}
