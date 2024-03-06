47a79ea3a145b6153034531dba977c1a76eb883b
lee <dlcksgur0919@naver.com>
2024-03-06 18:21:54 +0900

    💄 역사사진 슬라이드 구현 및 네비탭 추가

--- src/components/popup/historyP/HistoryPoPComponent/HistoryCom.js
+++ src/components/popup/historyP/HistoryPoPComponent/HistoryCom.js
@@ -1,28 +1,66 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import { useScroll } from 'framer-motion';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./History.css";
 
-const HistoryCom = ({ imageUrl, content }) => {
+const AnimatedText = ({ children }) => {
     const { scrollYProgress } = useScroll();
+    
+    return (
+        <motion.div style={{ opacity: scrollYProgress }}>
+            {children}
+        </motion.div>
+    );
+};
 
+
+const HistoryCom = ({ imageUrl, content }) => {
+    const sliderSettings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
     return (
         <div>
+            <Navbar className='nav' bg="dark" variant="dark" fixed="top">
+                <Container>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#">상세설명</Nav.Link>
+                        <Nav.Link href="#">사진</Nav.Link>
+                        <Nav.Link href="#">youtube</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
             <div className="main-bg">
                 <div className="wrapper">
-                    <img src={imageUrl} width="25%" />
+                    <Slider className='photo' {...sliderSettings}>
+                        {imageUrl.map((image, index) => (
+                            <div key={index} className="history-slide">
+                                <motion.img 
+                                    src={image} 
+                                    alt="History" 
+                                    className="history-image" 
+                                />
+                            </div>
+                        ))}
+                    </Slider>
                     <div className="memo">
                         <div className="wrap">
-                            <motion.div className="bar" style={{ scaleX: scrollYProgress }}>
-                                {content.map((paragraph, index) => (
-                                    <p key={index}>{paragraph}</p>
-                                ))}
-                            </motion.div>
+                            {content.map((paragraph, index) => (
+                                <p key={index}>{paragraph}</p>
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
     );
 };
 
--- src/components/popup/historyP/HistoryPop.js
+++ src/components/popup/historyP/HistoryPop.js
@@ -25,21 +25,24 @@ const movies = [
 ];
 
 const historyData = {
-  imageUrl: "https://upload.wikimedia.org/wikipedia/ko/2/2a/12%C2%B712_%EA%B5%B0%EC%82%AC%EB%B0%98%EB%9E%80_%EC%A3%BC%EB%8F%99%EC%9E%90.jpeg",
+  imageUrl:[ "https://upload.wikimedia.org/wikipedia/ko/2/2a/12%C2%B712_%EA%B5%B0%EC%82%AC%EB%B0%98%EB%9E%80_%EC%A3%BC%EB%8F%99%EC%9E%90.jpeg",
+  "https://img.movist.com/?img=/x00/04/93/47_p1.jpg","https://img.movist.com/?img=/x00/04/93/47_p1.jpg"],
   content: [
-      "Irure incididunt magna incididunt adipisicing nostrud exercitation consectetur non tempor. Reprehenderit cillum sint Lorem sunt labore magna. Labore cillum nulla laboris ex nostrud sit cillum. Magna consectetur adipisicing et nostrud pariatur pariatur. Veniam Lorem ea cupidatat ea qui laborum in proident nulla.",
-      "Fugiat ex elit pariatur irure occaecat. Voluptate occaecat consectetur ad aliquip quis tempor. Velit laboris do et eu nisi amet aliquip. Consectetur laboris reprehenderit commodo sint eiusmod laborum in. Excepteur consequat ut id nisi cillum sunt nisi eu ad laborum duis. Sunt Lorem esse cillum cillum nulla et occaecat Lorem reprehenderit minim consequat laborum reprehenderit consectetur.",
-      "Do cillum consectetur officia duis ex sit adipisicing laborum aliquip nulla deserunt sunt nostrud. Voluptate dolor tempor nisi proident anim. Sint occaecat et deserunt eiusmod non sit Lorem velit ut occaecat. Exercitation elit ut duis fugiat eiusmod duis ullamco aliquip. Fugiat velit exercitation elit eiusmod minim laborum consectetur et adipisicing cillum.",
-      "Ullamco do eu do Lorem commodo ullamco voluptate veniam. Elit nulla pariatur occaecat ex. Est commodo pariatur in ad ad id sit sint occaecat labore. Do laboris aliquip adipisicing amet laborum. Est qui aute anim nulla aliquip amet dolore est. Eu minim incididunt nulla elit. Irure id laborum exercitation dolor sunt sunt in eu cupidatat aliqua ex ipsum officia elit.",
-      "Nostrud elit excepteur quis ipsum dolore ea in cupidatat. Eu elit dolore pariatur laboris sunt cupidatat ad. Sint non cillum sit ullamco culpa ad aute mollit ex officia laborum voluptate amet minim. Sunt mollit enim est dolor nisi irure officia dolore ut do. Veniam non culpa eu et occaecat laborum dolore amet adipisicing. Elit fugiat deserunt mollit aute velit id occaecat labore. Exercitation non qui occaecat ea nisi sunt dolore incididunt labore.",
-      "Irure enim non sunt elit non excepteur exercitation et eiusmod ullamco excepteur labore est. Est in ipsum voluptate aliquip consectetur. Irure ullamco non elit exercitation consectetur pariatur id adipisicing duis ut aliquip ex nostrud. Veniam labore est elit amet ipsum anim non veniam. Culpa voluptate occaecat et cillum occaecat eiusmod nostrud sint aute mollit et ullamco. Mollit aute minim minim adipisicing Lorem mollit duis elit elit incididunt do. Deserunt dolore Lorem velit sunt adipisicing proident nulla ut."
-  ]
+
+      "1979년 10.26 사건 이후 새벽 4시 김재규가 체포된 후 국방부 회의실에서 각료들은 서둘러 비상조치를 내렸다. 신현확은 대통령이 서거했고 그 범인은 김재규였다는 것을 알리며, 국가적 위기 상황에 국가원수직이 비어있으면 안 된다고 말했다. 그러면서 법에 의하면 대통령 유고 시 국무총리가 승계하도록 돼 있으니, 지금부터 최규하 국무총리가 대통령 권한대행이 되어야 한다고 주장했다. 이 제안을 받아들여 최규하 국무총리가 대통령 권한대행이 되었고, 박정희 대통령 시해로 인한 계엄의 선포로 인해 정승화 육군참모총장이 계엄사령관으로 임명되어 대통령 권한대행과 함께 정국을 이끌게 되었다.",
+      "그 와중에 대통령 권한대행이 된 최규하는 철저한 원칙론자로, 정치에 휘말리기 싫어서 권력이 자신에게 집중되는 것을 스스로 막았다.[33] 실제로 권한대행 당시에 정승화가 계엄사의 업무를 어떻게 해야 하냐고 물어보자, 지금은 전국 계엄이 아니라 부분 계엄이니 자신한테 보고하지 말고 국방부 장관에게 보고하라 할 정도로 정치에 무관심했다. 이로 인해 계엄사령부 합동수사본부장으로 임명되어 10.26 사태 수사를 총괄하게 된 국군보안사령관 전두환이 군부의 실력자로 부상하게 된다.[34] 그리고 계엄사령관 정승화 입장에서 전두환은 곧 숙청해야 할 대상이 되었다.",
+      "최규하는 이 때 결정적인 실수를 되는데, 권한 대행 수락 당시 지금 이 순간부터 제주도를 제외한 전국에 비상계엄을 선포하겠다.라고 부분계엄을 선언 해버린다. 이 조치는 사실상 최규하 스스로 대통령 권한대행으로서의 군 통제권을 포기한다는 뜻이었다. 왜냐하면 당시 계엄법 9조에 의하면 전국을 계엄지역으로 선포하는 경우에는 대통령이 지휘감독을 하게 되지만 그 이외에는 국방부 장관이 지휘감독을 하기 때문이다. 원래 최규하는 정치에 휘말리기를 싫어했기에 정치적 부담감이 크다고 판단하여 제주도를 제외하고 계엄을 선포해버렸다. 이 때문에 계엄령 이후 '계엄사령부'가 유일한 권력의 중심이 되었고 계엄사령부 합동수사본부장으로 10.26 수사 총책을 맡은 전두환이 더더욱 권력의 핵심으로 부각됐다. 거기에 국방부 장관이었던 노재현은 그릇이 부족하고 전두환과 각별했기에, 훗날 정승화와 전두환이 권력투쟁을 벌일 때도 전두환을 두둔하기에 바빴다.",
+      "1979년 10.26 사건 이후 새벽 4시 김재규가 체포된 후 국방부 회의실에서 각료들은 서둘러 비상조치를 내렸다. 신현확은 대통령이 서거했고 그 범인은 김재규였다는 것을 알리며, 국가적 위기 상황에 국가원수직이 비어있으면 안 된다고 말했다. 그러면서 법에 의하면 대통령 유고 시 국무총리가 승계하도록 돼 있으니, 지금부터 최규하 국무총리가 대통령 권한대행이 되어야 한다고 주장했다. 이 제안을 받아들여 최규하 국무총리가 대통령 권한대행이 되었고, 박정희 대통령 시해로 인한 계엄의 선포로 인해 정승화 육군참모총장이 계엄사령관으로 임명되어 대통령 권한대행과 함께 정국을 이끌게 되었다.",
+      "그 와중에 대통령 권한대행이 된 최규하는 철저한 원칙론자로, 정치에 휘말리기 싫어서 권력이 자신에게 집중되는 것을 스스로 막았다.[33] 실제로 권한대행 당시에 정승화가 계엄사의 업무를 어떻게 해야 하냐고 물어보자, 지금은 전국 계엄이 아니라 부분 계엄이니 자신한테 보고하지 말고 국방부 장관에게 보고하라 할 정도로 정치에 무관심했다. 이로 인해 계엄사령부 합동수사본부장으로 임명되어 10.26 사태 수사를 총괄하게 된 국군보안사령관 전두환이 군부의 실력자로 부상하게 된다.[34] 그리고 계엄사령관 정승화 입장에서 전두환은 곧 숙청해야 할 대상이 되었다.",
+      "최규하는 이 때 결정적인 실수를 되는데, 권한 대행 수락 당시 지금 이 순간부터 제주도를 제외한 전국에 비상계엄을 선포하겠다.라고 부분계엄을 선언 해버린다. 이 조치는 사실상 최규하 스스로 대통령 권한대행으로서의 군 통제권을 포기한다는 뜻이었다. 왜냐하면 당시 계엄법 9조에 의하면 전국을 계엄지역으로 선포하는 경우에는 대통령이 지휘감독을 하게 되지만 그 이외에는 국방부 장관이 지휘감독을 하기 때문이다. 원래 최규하는 정치에 휘말리기를 싫어했기에 정치적 부담감이 크다고 판단하여 제주도를 제외하고 계엄을 선포해버렸다. 이 때문에 계엄령 이후 '계엄사령부'가 유일한 권력의 중심이 되었고 계엄사령부 합동수사본부장으로 10.26 수사 총책을 맡은 전두환이 더더욱 권력의 핵심으로 부각됐다. 거기에 국방부 장관이었던 노재현은 그릇이 부족하고 전두환과 각별했기에, 훗날 정승화와 전두환이 권력투쟁을 벌일 때도 전두환을 두둔하기에 바빴다."
+    ]
+
 };
 
 function HistoryPop() {
   return (
     <div className="HistoryPop">
-      <History imageUrl={historyData.imageUrl} content={historyData.content} />
+      <HistoryCom imageUrl={historyData.imageUrl} content={historyData.content} />
       
       <MovieCom movies={movies} />
 
