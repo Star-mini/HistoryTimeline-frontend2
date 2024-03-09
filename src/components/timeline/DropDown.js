4e07066c3bc8b34927a431e9b307aed9f6f49927
LeeJinyoung218 <amy021899@gmail.com>
2024-03-09 22:26:17 +0900

    :bulb: timeline component 설명 주석 추가

--- src/components/timeline/DropDown.js
+++ src/components/timeline/DropDown.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import '../../styles/timeline/dropdown.css';
 
 const DropDown = ({defaultLabel, valueList, value, setValue}) => {
+    // dropdown의 select option을 보여줄 지 말지의 state
     const [isVisible, setIsVisible] = useState(false);
 
+    // dropdown을 선택하면 invert
     const clickDropDown = () => {
         setIsVisible(!isVisible)
     }
 
+    // option 클릭하면 value를 option으로 바꾸고 option 닫기
     const clickDropDownOption = (option) => {
         setIsVisible(false);
         setValue(option);
--- src/components/timeline/ScrollLoadingBox.js
+++ src/components/timeline/ScrollLoadingBox.js
@@ -3,17 +3,20 @@ import '../../styles/timeline/scrollLoadingBox.css';
 import {useEffect, useState} from "react";
 
 const ScrollLoadingBox = ({isLoading}) => {
+    // loading bar의 애니메이션을 위한 width 설정
     const [width,  setWidth] = useState(0);
 
+    // loading 중에만 보이도록 한다.
     const display = {
         display: isLoading ? 'flex' : 'none'
     }
 
+    // 일정 시간마다 길이가 바뀌는 애니메이션 삽입
     useEffect(() => {
         const intervalId = setInterval(() => {
             // Update the width every second (or adjust the interval as needed)
             setWidth(prevWidth => (prevWidth < 150 ? prevWidth + 10 : 0));
-        }, 80); // 1000 milliseconds = 1 second
+        }, 80);
 
         return () => clearInterval(intervalId);
     }, []);
