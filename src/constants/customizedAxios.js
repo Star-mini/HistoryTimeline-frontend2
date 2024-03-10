c8db4f0c90059fd874d60c97b0ba2f0c3d7d9878
LeeJinyoung218 <amy021899@gmail.com>
2024-03-10 19:03:09 +0900

    :sparkles: react와 springboot 연결

--- src/constants/customizedAxios.js
+++ src/constants/customizedAxios.js
@@ -1,1 +1,6 @@
-export const { url } = "http://localhost:8080";
+import axios from 'axios';
+
+export const cusomizedAxios = axios.create({
+    baseURL: 'http://localhost:8080',
+    // 다른 옵션들도 필요에 따라 추가 가능
+});
