756da4ec19d894536d5b8ffdb9698b170d4c77ac
minsun <elinnane0@gmail.com>
2024-03-10 22:14:16 +0900

    ✨  메인화면의 타임라인 구현

--- src/components/main/MapTimeLine.js
+++ src/components/main/MapTimeLine.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
-import styles from "./TimeLine.scss";
+import "./TimeLine.scss";
+
 const useDetectScroll = () => {
     const [progressWidth, setProgressWidth] = useState(0);
 
@@ -10,8 +11,8 @@ const useDetectScroll = () => {
             document.documentElement.clientHeight;
         const scrollPercentage = (totalScroll / windowHeight) * 100;
 
-        const section = Math.floor(scrollPercentage / (100 / 6));
-        const width = section * (100 / 6);
+        const section = Math.min(4, Math.floor(scrollPercentage / (100 / 5)));
+        const width = (section+1) * (100 / 5);
         setProgressWidth(width);
     }, []);
 
@@ -26,34 +27,56 @@ const useDetectScroll = () => {
     return { progressWidth };
 };
 
-
 const MapTimeLine = () => {
     const { progressWidth } = useDetectScroll();
 
-    const isDotVisible = progressWidth !== 0 && progressWidth !== 100;
-    const dotLeft = `calc(${progressWidth}% + 20px)`;
+    const dots = [1, 2, 3, 4].map((index) => {
+        const dotPosition = (index / 5) * 100;
+        return (
+            <div
+                key={index}
+                className="dot"
+                style={{ left: dotPosition + "%",
+                backgroundColor: progressWidth >= dotPosition ? "#5cffd1" : "#deb488", }}
+            />
+        );
+    });
+
+    const col2Divs = [1, 2, 3, 4].map((index) => {
+        const Left = (index / 5) * 100 + 20;
+        return (
+            <div
+                key={index}
+                className="col-4"
+                style={{ left: Left + "%" }}
+            >
+                <p>test title {index}</p>
+                <p>test text</p>
+            </div>
+        );
+    });
+    
 
     return (
-        <div className="scrollable-container">
-            <div className="scrollbar">
-                <div style={{ overflowX: "auto" }}>
-                    <div className="progress-container">
-                        <div
-                            className="progress"
-                            style={{ width: progressWidth + "%" }}
-                        />
-                        {isDotVisible && (
+        <>
+            <div className="scrollable-container">
+                <div className="scrollbar">
+                    <div style={{ overflowX: "auto" }}>
+                        <div className="progress-container">
                             <div
-                                className="dot"
-                                style={{ left: progressWidth + "%"}}
+                                className="progress"
+                                style={{ width: progressWidth + "%" }}
                             />
-                        )}
+                            {dots}
+                            <div className="row">
+                                <div className="col-4"></div>
+                                {col2Divs}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
-
 
 export default MapTimeLine;
