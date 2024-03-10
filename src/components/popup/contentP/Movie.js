24f7d7a9d8bd07819da193c182d6dc463bafa838
Sang Min Park <minisstar@naver.com>
2024-03-10 14:22:46 +0900

    :sparkle: 댓글ui 라운드 추가

--- src/components/popup/contentP/Movie.js
+++ src/components/popup/contentP/Movie.js
@@ -1,18 +1,20 @@
 import '../../../styles/contents/movie.css';
 
 function Movie(props) {
+    // className prop을 추가하여 외부에서 전달받은 클래스를 적용합니다.
+    const { src, name, className } = props; // 구조 분해 할당을 사용하여 props를 추출합니다.
     return (
-        <div class="movie card movie-specific movie-background">
+        <div className={`movie card movie-specific movie-background ${className}`}>
           <img
-            src={props.src}
-            class="card-img-top"
+            src={src}
+            className="card-img-top" // class -> className으로 변경
             alt="..."
           />
-          <div class="card-body">
-            <h5 class="card-title">{props.name}</h5>
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
           </div>
         </div>
     );
-  }
+}
 
-    export default Movie;
+export default Movie;
--- src/components/popup/contentP/MovieCard.js
+++ src/components/popup/contentP/MovieCard.js
@@ -1,15 +1,18 @@
-// MovieCard.js
 import React from "react";
 import Moovie from "./Movie";
 import Fade from "react-reveal/Fade";
 
 const MovieCard = () => {
   return (
-    <div className="row row-cols-1 row-cols-sm-3 row-cols-md-3 g-3 mb-10">
+    <div className="row row-cols-1 row-cols-sm-3 row-cols-md-3 g-3 mb-10 movie-container">
       <Fade bottom delay={500}>
         <Moovie name="태극기휘날리며" src="태극기휘날리며.jpg" />
+      </Fade>
+      <Fade bottom delay={500}>
         <Moovie name="태극기휘날리며" src="태극기휘날리며.jpg" />
-        <Moovie name="태극기휘날리며" src="태극기휘날리며.jpg" />        
+      </Fade>
+      <Fade bottom delay={500}>
+        <Moovie name="태극기휘날리며" src="태극기휘날리며.jpg" className="movie-right" />
       </Fade>
     </div>
   );
