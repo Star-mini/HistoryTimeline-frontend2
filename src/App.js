e51dee977f528212a96deed05edf22f18d467c46
minsun <elinnane0@gmail.com>
2024-03-13 22:47:33 +0900

    💩 테스트용 App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ContentsPopup from "./components/popup/contentP/ContentsPopup";
+//import ContentsPopup from "./components/popup/contentP/ContentsPopup";
 import VideoZoomInOut from "./components/main/VideoZoomInOut";
 import MapMain from "./components/main/MapMain";
 import AdminInsert from "./components/admain/insert/AdminInsert";
-import Timeline from "./pages/Timeline";
+import MapChartV2 from "./components/main/MapChartV2";
+// import Timeline from "./pages/Timeline";
 
 /*
  * "/" 동영상 있는 메인페이지
@@ -17,9 +18,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<VideoZoomInOut />} />
                 <Route path="/map" element={<MapMain />} />
+                <Route path="/mapv2" element={<MapChartV2 />} />
                 <Route path="/adminInsert" element={<AdminInsert/>} />
-                <Route path="/timeline" element={<Timeline />} />
-                <Route path="/contents" element={<ContentsPopup />} />
+                {/* <Route path="/timeline" element={<Timeline />} /> */}
+                {/* <Route path="/contents" element={<ContentsPopup />} /> */}
             </Routes>
         </Router>
     );
