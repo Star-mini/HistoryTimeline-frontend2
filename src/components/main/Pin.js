49aafa7eff9c878ab95b9047a7cd4b68d2ec9ea3
minsun <elinnane0@gmail.com>
2024-03-18 01:23:21 +0900

    💄 핀 스타일 변경 및 클릭 이벤트 적용

--- src/components/main/Pin.js
+++ src/components/main/Pin.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { animated } from "react-spring";
+import { useNavigate } from "react-router-dom";
+import "./Pin.scss";
 
-function Pin({x, y}) {
+function Pin({ x, y, country }) {
+
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/timeline", { state: { country } }); 
+    };
 
     return (
-        <animated.div
+        <animated.div className="mapPin"
             style={{
                 position: "absolute",
                 width: "10px",
                 height: "10px",
                 borderRadius: "50%",
-                background: "red",
-                transform: `translate(${x}vw, ${y}vw)`, 
+                transform: `translate(${x}vw, ${y}vw)`,
+                cursor: "pointer",
             }}
+            onClick={handleClick}
         />
     );
 }
