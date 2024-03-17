aa336ff6afed63d06fd8fe00cb459de187a48d7a
minsun <elinnane0@gmail.com>
2024-03-17 23:26:06 +0900

    💄  메인 스타일 변경

--- src/components/main/MapMain.js
+++ src/components/main/MapMain.js
@@ -1,21 +1,18 @@
 import React from "react";
 import MapChartV2 from "./MapChartV2";
 import Header from "../Header";
-import MapTimeLine from "./MapTimeLine";
+import MapTimeLineV2 from "./MapTimeLineV2";
 import Footer from "../Footer";
 
 /**
  * 지도 메인화면
  */
 function MapMain() {
-    const backColor = "#264364"; // 배경색
     return (
         <div
             style={{
-                paddingLeft: "50px",
-                paddingRight: "50px",
                 textAlign: "center",
-                height: "2000px",
+                height: "3000px",
                 background: "linear-gradient(45deg, rgb(209, 202, 252), rgb(234, 226, 253))"
             }}
         >
@@ -23,22 +20,16 @@ function MapMain() {
             <br />
             <div
                 style={{
-                    justifyContent: "center",
-                    alignItems: "center",
                     margin : "auto",
                     textAlign: "center",
-                    width: "80%",
+                    width: "100%",
                     height: "80vh",
                     position: "fixed",
                     top: "120px",
-                    left: "150px",
                 }}
             >
                 <MapChartV2 />
             </div>
-            {/* <br /><br />
-            <MapTimeLine />
-            <br /><br /> */}
             <Footer/>
         </div>
     );
