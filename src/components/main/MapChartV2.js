a2d49d620e38fcb4cbb8059ab24d5107007cfb19
minsun <elinnane0@gmail.com>
2024-03-17 23:25:36 +0900

    ♻️ 메인 지도 배치 수정 및 타임라인 배치

--- src/components/main/MapChartV2.js
+++ src/components/main/MapChartV2.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
 import Pin from "./Pin.js";
+import MapTimeLineV2 from "./MapTimeLineV2.js";
 
 function MapChartV2() {
     const [scrollY, setScrollY] = useState(window.scrollY);
+    const [showTimeline, setShowTimeline] = useState(false);
     const [pins, setPins] = useState([
-        { x:"30%", y: "20%"},
+        { x: "30", y: "10" },
         //{ x: "10", y: "10" },
     ]);
 
     useEffect(() => {
         const handleScroll = () => {
             setScrollY(window.scrollY);
+            if (window.scrollY > 800) {
+                setShowTimeline(true);
+            } else {
+                setShowTimeline(false);
+            }
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -30,15 +37,15 @@ function MapChartV2() {
 
     const westStyle = useSpring({
         top: scrollY <= maxScroll ? `${Math.min(scrollY, 5)}vw` : "5vw",
-        left: scrollY <= maxScroll ? `${Math.min(scrollY + 6, 12)}vw` : "12vw",
+        left: scrollY <= maxScroll ? `${Math.min(scrollY + 6, 10)}vw` : "10vw",
     });
 
     const eastStyle = useSpring({
         top: scrollY <= maxScroll ? `${Math.min(scrollY, 5)}vw` : "5vw",
-        right: scrollY <= maxScroll ? `${Math.min(scrollY + 5, 12)}vw` : "12vw",
+        right: scrollY <= maxScroll ? `${Math.min(scrollY + 5, 9)}vw` : "9vw",
     });
 
-    const containerStyle = useSpring({
+    const earthBoxStyle = useSpring({
         top: scrollY <= maxScroll ? `${Math.min(scrollY, 1)}vw` : "1vw",
         right: scrollY <= maxScroll ? `${Math.min(scrollY, 5)}vw` : "5vw",
         width:
@@ -56,84 +63,96 @@ function MapChartV2() {
     });
 
     return (
-        <div
-            className="mapBox"
-            style={{
-                maxWidth: "60vw",
-                width: "60vw",
-                height: `${mapBoxHeight}px`,
-                margin: "0 auto",
-                position: "relative",
-                textAlign: "center",
-                display: "flex",
-                justifyContent: "center",
-                overflow: "hidden",
-            }}
-        >
-            <div
-                className="map"
-                style={{
-                    textAlign: "center",
-                    maxWidth: "65vw",
-                    width: "65vw",
-                    position: "absolute",
-                    height: "auto",
-                    zIndex: 1,
-                    ...mapBoxStyle,
-                }}
-            >
-                <animated.img
-                    src="./western.svg"
-                    alt="west"
+        <div class="container">
+            <div className="row" style={{justifyContent: "center"}}>
+                <div
+                    className="col-7"
                     style={{
-                        borderRadius: "5vw",
-                        width: "15vw",
-                        position: "absolute",
-                        ...westStyle,
+                        maxWidth: "60vw",
+                        width: "60vw",
+                        height: `${mapBoxHeight}px`,
+                        margin: "0 0 0 0",
+                        position: "relative",
+                        textAlign: "center",
+                        display: "flex",
+                        justifyContent: "center",
+                        overflow: "hidden",
                     }}
-                />
-                <animated.img
-                    src="./eastern.svg"
-                    alt="east"
-                    style={{
-                        width: "30vw",
-                        position: "absolute",
-                        ...eastStyle,
-                    }}
-                />
-                <div>
-                    {pins.map((pin, index) => (
-                        <Pin
-                            key={index}
-                            x={pin.x}
-                            y={pin.y}
-                            imageOffset={{ left: 0, top: 0 }}
+                >
+                    <div
+                        className="map"
+                        style={{
+                            zIndex: 1,
+                            ...mapBoxStyle,
+                            width: "40vw",
+                            height: "40vw"
+                        }}
+                    >
+                        <animated.img
+                            src="./western.svg"
+                            alt="west"
+                            style={{
+                                borderRadius: "5vw",
+                                width: "15vw",
+                                position: "absolute",
+                                ...westStyle,
+                            }}
                         />
-                    ))}
-                </div>
-            </div>
+                        <animated.img
+                            src="./eastern.svg"
+                            alt="east"
+                            style={{
+                                width: "30vw",
+                                position: "absolute",
+                                ...eastStyle,
+                            }}
+                        />
+                        <div>
+                            {pins.map((pin, index) => (
+                                <Pin
+                                    key={index}
+                                    x={pin.x}
+                                    y={pin.y}
+                                    imageOffset={{ left: 0, top: 0 }}
+                                    style
+                                />
+                            ))}
+                        </div>
+                    </div>
 
-            <div
-                className="earth"
-                style={{
-                    width: "50vw",
-                    height: "40vw",
-                    borderRadius: "50%",
-                    position: "absolute",
-                    zIndex: 0,
-                }}
-            >
-                <animated.div
-                    className="container"
+                    <div
+                        className="earth"
+                        style={{
+                            width: "50vw",
+                            height: "40vw",
+                            borderRadius: "50%",
+                            position: "absolute",
+                            zIndex: 0,
+                        }}
+                    >
+                        <animated.div
+                            className="earthBox"
+                            style={{
+                                background: "#eaeafc",
+                                position: "absolute",
+                                borderRadius: "5vw",
+                                width: "50vw",
+                                height: "40vw",
+                                ...earthBoxStyle,
+                            }}
+                        ></animated.div>
+                    </div>
+                </div>
+                <div
+                    className="col-5"
                     style={{
-                        background: "#eaeafc",
-                        position: "absolute",
-                        borderRadius: "5vw",
-                        width: "50vw",
-                        height: "40vw",
-                        ...containerStyle,
+                        display: showTimeline ? "flex" : "none",
+                        width:"20vw",
+                        textAlign: "center"
                     }}
-                ></animated.div>
+                >
+                    <MapTimeLineV2 />
+                </div>
             </div>
         </div>
     );
