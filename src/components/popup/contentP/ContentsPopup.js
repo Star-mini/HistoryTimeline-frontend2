9e65897a0f5ed7c15e8cbfb7562f08d2b0285cbb
Sang Min Park <minisstar@naver.com>
2024-03-22 03:20:16 +0900

    ✨ contendid 넘겨주기

--- src/components/popup/contentP/ContentsPopup.js
+++ src/components/popup/contentP/ContentsPopup.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Comments from './Comments';
 import MovieCard from './MovieCard';
 import Description from './Description';
 import SectionComponent from './SectionComponent';
 import Detail from './Detail';
-import '../../../styles/contents/ContentsPopup.css'// ContentsPopup.css 파일을 임포트합니다.
-
+import '../../../styles/contents/ContentsPopup.css';
 
 function ContentsPopup() {
+  const contentId = "550"; // 여기에 해당 영화의 ID를 설정
+
   return (
     <div className="ContentsPopup">
       <div>
-        <Description />
+        <Description contentId={contentId}/>
         <div style={{ marginTop: "15px" }}>
-        <SectionComponent />
+          <SectionComponent contentId={contentId}/>
         </div>
         <div className="movie-card-margin">
-          <MovieCard />
+          <MovieCard contentId={contentId}/>
         </div>
-        <Detail />
+        <Detail contentId={contentId} /> 
         <div style={{ marginTop: "80px" }}>
-          <Comments />
+          <Comments contentId={contentId}/>
         </div>
       </div>
     </div>
