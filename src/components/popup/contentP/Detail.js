96f0503c493231b3bb3759aad0cb6c4b7520a619
Sang Min Park <minisstar@naver.com>
2024-03-22 03:21:57 +0900

    ✨ 세부사항Tmdb연결

--- src/components/popup/contentP/Detail.js
+++ src/components/popup/contentP/Detail.js
@@ -1,24 +1,44 @@
-// Detail.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../../../styles/contents/detail.css';
 
-const Detail = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
-  
-    return (
-      <section className="detail-section">
-        <h3 className="detail-title">콘텐츠 설명</h3>
-        <div id="detailContent" className={`detail-content ${isExpanded ? 'detail-expanded' : 'detail-collapsed'}`}>
-          <p>
-            {/* 콘텐츠 내용 */}
-            크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램크리에이터 킴이안, 이안스다양한 이유로 이별한 커플들이 한 장에 모여 지나간 연애를 되짚고 새로운 연인을 만족하며 자신만의 사랑을 찾아가는 여행 리얼리티 프로그램
-          </p>
-        </div>
+const Detail = ({ contentId }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [details, setDetails] = useState('');
+
+  const api_key = "0decfffb82411d82c9af75fdfaba9b34";
+  const baseUrl = "https://api.themoviedb.org/3";
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/movie/${contentId}?api_key=${api_key}&language=ko-KR`);
+        setDetails(response.data.overview);
+      } catch (error) {
+        console.error('영화 상세 정보를 가져오는 데 실패했습니다.', error);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [contentId]);
+
+  const shouldShowButton = details.length >310;
+
+  return (
+    <section className="detail-section">
+      <h3 className="detail-title">콘텐츠 설명</h3>
+      <div id="detailContent" className={`detail-content ${isExpanded ? 'detail-expanded' : 'detail-collapsed'}`}>
+        <p>
+          {details}
+        </p>
+      </div>
+      {shouldShowButton && (
         <button id="detailToggle" className="detail-button" onClick={() => setIsExpanded(!isExpanded)}>
           {isExpanded ? '접기 ▲' : '더보기 ▼'}
         </button>
-      </section>
-    );
-  };
-  
-  export default Detail;
+      )}
+    </section>
+  );
+};
+
+export default Detail;
