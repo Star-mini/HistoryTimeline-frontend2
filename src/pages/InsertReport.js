4e2673f84090bc240b31c0be82ec35f4bf55be56
LeeJinyoung218 <amy021899@gmail.com>
2024-03-22 01:24:07 +0900

    :lipstick: HistoryReport 저장 페이지 디자인

--- src/pages/InsertReport.js
+++ src/pages/InsertReport.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {cusomizedAxios as axios} from "../constants/customizedAxios";
+import "../styles/report/report.css"
 
 // text로 제보를 받는 페이지
 const InsertReport = () => {
@@ -20,16 +21,21 @@ const InsertReport = () => {
     const onClickReportButton = async () => {
         const confirmed = window.confirm("제보하시겠습니까?");
         if (confirmed) {
-            try {
-                await axios.post(`/report/save`, {
-                    userId: 1, // login 인증 후 JWT에서 userId값을 가져와야함.
-                    report: report
-                });
-                window.history.back();
-                setReport("");
-            } catch (error) {
-                console.error("Error saving data:", error);
-                window.confirm("다시한번 시도해보세요.");
+            if (report === "") {
+                window.alert("내용을 적어주세요.");
+            }
+            else {
+                try {
+                    await axios.post(`/report/save`, {
+                        userId: 1, // login 인증 후 JWT에서 userId값을 가져와야함.
+                        report: report
+                    });
+                    window.history.back();
+                    setReport("");
+                } catch (error) {
+                    console.error("Error saving data:", error);
+                    window.confirm("다시한번 시도해보세요.");
+                }
             }
         } else {
             window.confirm("다시한번 시도해보세요.");
@@ -40,11 +46,16 @@ const InsertReport = () => {
         <div className="insert-report-background">
             <div className="insert-report-container">
                 <span className="insert-report-title">제보</span>
-                <input type="text" value={report} onChange={handleInputChange}/>
-                <button className="button cancel-button"
-                    onClick={() => onClickCancelButton()}>취소</button>
-                <button className="button report-button"
-                    onClick={() => onClickReportButton()}>제출</button>
+                <textarea className="insert-report-textarea" value={report} onChange={handleInputChange}
+                    placeholder="제보할 역사에 대해서 적어주세요."/>
+                <div className="insert-report-buttons">
+                    <button className="button cancel-button"
+                            onClick={() => onClickCancelButton()}>취소
+                    </button>
+                    <button className="button report-button"
+                            onClick={() => onClickReportButton()}>제출
+                    </button>
+                </div>
             </div>
         </div>
     );
