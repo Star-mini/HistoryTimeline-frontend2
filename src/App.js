cabc9673a3569a79a8280463f2bf2df61a9205d5
LeeJinyoung218 <amy021899@gmail.com>
2024-03-23 16:02:43 +0900

    :sparkles: 지도 페이지 pin과 타임라인 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import VideoZoomInOut from "./pages/VideoZoomInOut";
 import MapMain from "./pages/MapMain";
@@ -7,6 +7,7 @@ import Timeline from "./pages/Timeline";
 import AdminList from "./pages/AdminList";
 import Login from "./components/login/login";
 import InsertReport from "./pages/InsertReport";
+import Test from "./components/popup/historyP/Test";
 
 /*
  * "/" 동영상 있는 메인페이지
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/" element={<VideoZoomInOut />} />
                 <Route path="/map" element={<MapMain />} />
                 <Route path="/adminList" element={<AdminList />} />
+                <Route path="/timeline/:countryId" element={<Timeline />} />
                 <Route path="/timeline" element={<Timeline />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/timeline" element={<Timeline />} />
