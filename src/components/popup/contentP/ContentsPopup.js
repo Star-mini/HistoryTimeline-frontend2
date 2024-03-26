b4b783ce7c1ea37684bb5fc5a2316788ad625357
Sang Min Park <minisstar@naver.com>
2024-03-26 19:17:28 +0900

    💄 책넘김 애니메이션 삭제

--- src/components/popup/contentP/ContentsPopup.js
+++ src/components/popup/contentP/ContentsPopup.js
@@ -9,27 +9,15 @@ import Detail from "./Detail";
 import "../../../styles/contents/ContentsPopup.css";
 import FetchMovieID from "./FetchMovieID";
 
-function ContentsPopup({ movieTitle = "택시운전사", onClose }) {
+function ContentsPopup({ movieTitle = "태극기 휘날리며", onClose }) {
   const [contentId, setContentId] = useState("");
   const [title, setTitle] = useState(movieTitle); // movieTitle 상태 추가
   const popupRef = useRef(null); // DOM 요소에 대한 참조를 생성합니다.
-
-
 
   // contentId 상태가 변경될 때마다 콘솔에 출력
   useEffect(() => {
     console.log(`contentId가 변경되었습니다: ${contentId}`);
   }, [contentId]);
-
-  useEffect(() => {
-    // title 상태가 변경될 때마다 실행할 작업
-    if (popupRef.current) {
-      popupRef.current.classList.add("pageTurnAnimation");
-      setTimeout(() => {
-        popupRef.current.classList.remove("pageTurnAnimation");
-      }, 1000); // 애니메이션 지속 시간과 동일하게 설정
-    }
-  }, [title]);
 
   // URL에서 title 파라미터를 추출하는 대신, movieTitle 프롭스를 사용합니다.
   const handleMovieIdFetched = (id) => {
@@ -42,7 +30,6 @@ function ContentsPopup({ movieTitle = "택시운전사", onClose }) {
     setTitle(selectedTitle);
   };
 
-
   return (
     <div className="popup" ref={popupRef}>
       <FetchMovieID movieTitle={title} onMovieIdFetched={handleMovieIdFetched} />
@@ -53,11 +40,11 @@ function ContentsPopup({ movieTitle = "택시운전사", onClose }) {
           <PlatformSection contentId={contentId} />
         </div>
         <div className="movie-card-margin">
-        <ContentCard contentId={contentId} onContentSelect={handleContentSelect} />
+          <ContentCard contentId={contentId} onContentSelect={handleContentSelect} />
         </div>
         <Detail contentId={contentId} />
         <div style={{ marginTop: "80px" }}>
-          <Comments movieTitle={title} /> {/* useParams 대신 movieTitle 프롭스를 사용합니다. */}
+          <Comments contentId={contentId}/> {/* useParams 대신 movieTitle 프롭스를 사용합니다. */}
         </div>
       </div>
     </div>
