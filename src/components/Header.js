301b8d1bb552af35253fd244b03e3e5051f876ae
minsun <elinnane0@gmail.com>
2024-03-27 15:44:02 +0900

    ♻️ 로그인 로그아웃 표시 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/font.css";
+import cookie from "react-cookies";
 
 function Header(props) {
-    const fontColor = "#0d0d0d"; // 글씨
-    const propsBackground = props.background;
-    console.log(props);
-    return (
-        <div
-            className="header container"
-            style={{
-                margin: 0,
-                maxWidth: "100%",
-                position: "fixed",
-                background : propsBackground || "" 
-            }}
+  const fontColor = "#0d0d0d"; // 글씨
+  const propsBackground = props.background;
+  const [state, setState] = useState("");
+  useEffect(() => {
+    setState(cookie.load("email"));
+    console.log(state);
+  }, []);
+
+  const isLoginState = () => {
+    return state != null;
+  };
+  const LoginAndLogout = () => {
+    if (isLoginState()) {
+      return (
+        <a
+          className="nav-link"
+          href="#"
+          style={{
+            color: fontColor,
+            paddingRight: "15px",
+            fontSize: "15pt",
+          }}
         >
-            <div className="row">
-                <div className="col-4">
-                    <img src="logo.png" style={{ width: "90px", height: "90px" }}></img>
-                </div>
-                <div className="col-8">
-                    <ul className="nav justify-content-end">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="/login"
-                                style={{
-                                    color: fontColor,
-                                    paddingRight: "15px",
-                                    fontSize: "15pt",
-                                }}
-                            >
-                                Login
-                            </a>
-                        </li>
-                        <li className="nav-item ">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                style={{
-                                    color: fontColor,
-                                    paddingRight: "0px",
-                                    fontSize: "15pt",
-                                }}
-                            >
-                                Join
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          Logout
+        </a>
+      );
+    } else {
+      return (
+        <a
+          className="nav-link"
+          href="/login"
+          style={{
+            color: fontColor,
+            paddingRight: "15px",
+            fontSize: "15pt",
+          }}
+        >
+          Login
+        </a>
+      );
+    }
+  };
+  return (
+    <div
+      className="header container"
+      style={{
+        margin: 0,
+        maxWidth: "100%",
+        position: "fixed",
+        background: propsBackground || "",
+      }}
+    >
+      <div className="row">
+        <div className="col-4">
+          <img src="logo.png" style={{ width: "90px", height: "90px" }}></img>
         </div>
-    );
+        <div className="col-8">
+          <ul className="nav justify-content-end">
+            <li className="nav-item" style={{ top: "10px" }}>
+              {LoginAndLogout()}
+            </li>
+            <li className="nav-item ">
+              <a
+                className="nav-link"
+                href="#"
+                style={{
+                  color: fontColor,
+                  paddingRight: "0px",
+                  fontSize: "15pt",
+                }}
+              >
+                Join
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
