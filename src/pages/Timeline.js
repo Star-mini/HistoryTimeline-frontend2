b20970ee19217bcd5f25830dfa07adf0660b9eba
LeeJinyoung218 <amy021899@gmail.com>
2024-03-27 15:19:45 +0900

    :lipstick: Timeline Page에 header, footer 추가

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,13 +1,17 @@
 import TimelineComponent from "../components/timeline/Timeline";
 import "../styles/timeline/timeline.css"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 // 타임라인 페이지
 const Timeline = () => {
     return (
         <div>
+            <Header />
             {/* 타임라인 컴포넌트 */}
             <TimelineComponent />
+            <Footer />
         </div>
     );
 };
