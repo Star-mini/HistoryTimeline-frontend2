86350c0e03915dcc189c31428a082424343cee12
LeeJinyoung218 <amy021899@gmail.com>
2024-03-27 09:34:56 +0900

    :poop: cookie 인증 구현 중

--- src/pages/MapMain.js
+++ src/pages/MapMain.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MapChartV2 from "../components/main/MapChartV2";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/font.css"
+import cookie from 'react-cookies';
 
 /**
  * 지도 메인화면
  */
 function MapMain() {
+
+    const [email, setEmail] = useState("");
+
+    useEffect(() => {
+        setEmail(cookie.load('email'));
+        console.log(email);
+    }, [])
+
     return (
         <div
             style={{
