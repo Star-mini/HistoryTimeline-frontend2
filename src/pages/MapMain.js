b8d0653f4164d2932ec2059e0e9fbe47244d6f47
minsun <elinnane0@gmail.com>
2024-03-27 15:29:31 +0900

    💄 메인페이지 구조 변경

--- src/pages/MapMain.js
+++ src/pages/MapMain.js
@@ -1,47 +1,49 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import MapChartV2 from "../components/main/MapChartV2";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../styles/font.css"
-import cookie from 'react-cookies';
+import "../styles/font.css";
+import cookie from "react-cookies";
 
 /**
  * 지도 메인화면
  */
 function MapMain() {
+  const [email, setEmail] = useState("");
 
-    const [email, setEmail] = useState("");
+  useEffect(() => {
+    setEmail(cookie.load("email"));
+    console.log(email);
+  }, []);
 
-    useEffect(() => {
-        setEmail(cookie.load('email'));
-        console.log(email);
-    }, [])
-
-    return (
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          textAlign: "center",
+          height: "3000px",
+          background:
+            "linear-gradient(45deg, rgb(209, 202, 252), rgb(234, 226, 253))",
+        }}
+      >
+        <br />
         <div
-            style={{
-                textAlign: "center",
-                height: "3000px",
-                background: "linear-gradient(45deg, rgb(209, 202, 252), rgb(234, 226, 253))"
-            }}
+          style={{
+            margin: "auto",
+            textAlign: "center",
+            width: "100%",
+            height: "100vh",
+            position: "fixed",
+            top: "100px",
+          }}
         >
-            <Header />
-            <br />
-            <div
-                style={{
-                    margin : "auto",
-                    textAlign: "center",
-                    width: "100%",
-                    height: "100vh",
-                    position: "fixed",
-                    top: "100px",
-                }}
-            >
-                <MapChartV2 />
-            </div>
-            <Footer/>
+          <MapChartV2 />
         </div>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default MapMain;
