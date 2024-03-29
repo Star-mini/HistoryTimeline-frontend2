0ce60f43b9a8c962c619d4e61391f030ff1d85bd
Sang Min Park <minisstar@naver.com>
2024-03-29 20:55:51 +0900

    💄 보이는 영화갯수 수정

--- src/components/popup/contentP/ContentCard.js
+++ src/components/popup/contentP/ContentCard.js
@@ -11,7 +11,7 @@ const settings = {
   dots: true,
   infinite: true,
   speed: 700, // 슬라이드 전환 속도
-  slidesToShow: 5,
+  slidesToShow: 3,
   slidesToScroll: 2,  
   autoplay: true,
   autoplaySpeed: 3000, // 자동 전환 속도
