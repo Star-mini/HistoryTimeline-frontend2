6cf3513bdda36ca80c519d019b31a0f30adc530e
Sang Min Park <minisstar@naver.com>
2024-03-29 22:02:54 +0900

    💄 영화 목록 ui 수정

--- src/components/popup/contentP/ContentCard.js
+++ src/components/popup/contentP/ContentCard.js
@@ -17,39 +17,6 @@ const settings = {
   autoplaySpeed: 3000, // 자동 전환 속도
   cssEase: 'ease-in-out', // 부드러운 전환 효과를 위한 속도 곡선 설정
   responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        cssEase: 'ease-in-out',
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        cssEase: 'ease-in-out',
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        cssEase: 'ease-in-out',
-      },
-    },
   ],
 };
 
